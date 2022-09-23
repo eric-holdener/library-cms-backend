81bf9d3936dfa5d775f40ecaa25391c34c944391
EHoldener <eholdener12@gmail.com>
2022-09-23 15:10:09 -0700

    wrote migrations for books, reviews, comments, userbooks

--- db/migrate/20220922203002_create_user_books.rb
+++ db/migrate/20220922203002_create_user_books.rb
@@ -1,6 +1,8 @@
 class CreateUserBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :user_books do |t|
+      t.integer :user_id
+      t.integer :book_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_202800) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_203029) do
+  create_table "books", force: :cascade do |t|
+    t.string "isbn", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "score"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_202800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "reviews", "users"
 end
