a35b0af97339a39cdf7d8343a9c990f1f601af5c
EHoldener <eholdener12@gmail.com>
2022-09-22 20:24:00 -0700

    started scaffolding other db tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :reviews
+  resources :user_books
+  resources :books
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
