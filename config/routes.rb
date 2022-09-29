583d7a347ee89dabbdcc23fa47922d2318f40c34
EHoldener <eholdener12@gmail.com>
2022-09-29 13:44:53 -0700

    modified devise routes to use controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   resources :reviews
   resources :user_books
   resources :books
-  devise_for :users
+  devise_for :users,
+              controllers: {
+                sessions: 'users/sessions',
+                registrations: 'users/registrations'
+              }
+  get '/member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
